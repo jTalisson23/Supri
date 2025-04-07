ef661e8fa0934f8dcde2eab6ffc7050949e58e93
jTalisson23 <tropaszero@gmail.com>
2025-04-07 20:40:55 -0300

    Supri_Limpa

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText editTextUsername, editTextPassword;
+    Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario = editTextUsername.getText().toString();
+                String senha = editTextPassword.getText().toString();
+
+                if (usuario.equals("admin") && senha.equals("123")) {
+                    Toast.makeText(LoginActivity.this, "Login bem-sucedido!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Usuário ou senha incorretos.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
