5957478ffa8d8c13e062357d700b680e7e5acdee
GFTguilherme <uguilhermeteles@outlook.com>
2025-04-29 10:34:01 -0300

    tela de limpeza

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
