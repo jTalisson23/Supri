bd97a245608e5a618633e0ce693bb26a181e237b
jTalisson23 <tropaszero@gmail.com>
2025-05-01 14:31:36 -0300

    contribuicoes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,8 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-database")
-
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("com.github.bumptech.glide:compiler:4.16.0")
     // Outras libs
     implementation("com.squareup.picasso:picasso:2.71828")
     implementation("com.android.volley:volley:1.2.1")
--- app/src/main/java/com/example/myapplication/productsfa.java
+++ app/src/main/java/com/example/myapplication/productsfa.java
@@ -14,6 +14,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import androidx.recyclerview.widget.GridLayoutManager;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +32,7 @@ public class productsfa extends AppCompatActivity {
         setContentView(R.layout.fragment_products);
 
         recyclerView = findViewById(R.id.recyclerProdutos);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3)); // 3 colunas
 
         listaProdutos = new ArrayList<>();
         adapter = new ProdutoAdapter(listaProdutos, this);
