6846751b93c6a6dfbde6a7e2ba1fa4ad7d99bf85
jTalisson23 <tropaszero@gmail.com>
2025-05-13 12:09:49 -0300

    Minhas alterações locais em InicioActivity.java

--- app/src/main/java/com/example/myapplication/InicioActivity.java
+++ app/src/main/java/com/example/myapplication/InicioActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -102,6 +103,7 @@ public class InicioActivity extends AppCompatActivity {
 
         // Botão de acesso à tela de cadastro de produto
         Button btnAbrirCadastro = findViewById(R.id.btnAbrirCadastro);
+        btnAbrirCadastro.setVisibility(View.GONE); // faz sumir da tela
         btnAbrirCadastro.setOnClickListener(v -> {
             Intent intent = new Intent(InicioActivity.this, CadastroProdutoActivity.class);
             startActivity(intent);
